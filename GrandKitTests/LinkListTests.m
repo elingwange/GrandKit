1632c25f787888b8479373061fb1b3b910640e95
evan <elingwange@gmail.com>
2019-04-23 21:08:29 +0800

    [LinkList]添加链表的部分操作

--- GrandKitTests/LinkListTests.m
+++ GrandKitTests/LinkListTests.m
@@ -26,13 +26,17 @@ typedef Node LinkList;
 
 @implementation LinkListTests
 
-LinkList *creatLinkList2(int n) {
+LinkList *creatLinkList(int n) {
     Node *head, *tail;
     head = (Node *)malloc(sizeof(Node));
+//    NSLog(@"Tag %d", head->data);
+    
     tail = head;
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         Node *node = (Node *)malloc(sizeof(Node));
-        node->data = i + 1;
+        node->data = i;
+//        NSLog(@"Tag %d", node->data);
+        
         tail->next = node;
         tail = node;
     }
@@ -41,17 +45,82 @@ LinkList *creatLinkList2(int n) {
 }
 
 void printLinkList(LinkList *pList) {
-    while(pList->next != NULL) {
-        NSLog(@"Tag %d", pList->data);
-        pList = pList->next;
+    Node *p = pList;
+    while(p->next != NULL) {
+        NSLog(@"Tag %d", p->data);
+        p = p->next;
     }
+    NSLog(@"Tag %d", p->data);
+    NSLog(@"Tag ------------");
 }
 
+LinkList * insertToHead(LinkList *pList, Node *pNode) {
+    pNode->next = pList;
+    return pNode;
+}
+
+LinkList * insertToRear(LinkList *pList, Node *pNode) {
+    // 定义初始指向头部的尾指针
+    Node *pear = pList;
+    // 将尾指针移到尾部
+    while (pear->next != NULL) {
+        pear = pear->next;
+    }
+    pear->next = pNode;
+    pNode->next = NULL;
+    return pList;
+}
+
+LinkList * insertList(LinkList *list, Node *node, int position) {
+    
+    Node *pMoving = list;
+    // 将指针移动到指定位置的前一位
+    for (int i = 1; i < (position - 1); i++) {
+        pMoving = pMoving->next;
+    }
+    node->next = pMoving->next;
+    pMoving->next = node;
+    
+    return list;
+}
+
+LinkList * deleteList(LinkList *list, int position) {
+    Node *near = list;
+    for (int i = 1; i < (position - 1); i++) {
+        near = near->next;
+    }
+    // 此处对节点指针的释放的用法有问题
+//    Node *node = near->next;
+//    free(node);
+    near->next = near->next->next;
+    return list;
+}
 
 - (void)testLinkList {
 
-    LinkList *pList = creatLinkList2(5);
+    LinkList *pList = creatLinkList(5);
+//    printLinkList(pList);
+    
+    Node *node = (Node *)malloc(sizeof(Node));
+//    node->data = 11;
+//    pList = insertToHead(pList, node);
+//    printLinkList(pList);
+//
+//    // 头节点 表头节点
+//
+//    node = (Node *)malloc(sizeof(Node));
+//    node->data = 22;
+//    pList = insertToRear(pList, node);
     printLinkList(pList);
+    
+    node->data = 99;
+    pList = insertList(pList, node, 2);
+    printLinkList(pList);
+    
+    
+    pList = deleteList(pList, 2);
+    printLinkList(pList);
+    
 }
 
 @end
