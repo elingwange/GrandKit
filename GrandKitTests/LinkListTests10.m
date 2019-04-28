f1fd0088121da2bffeffdbfe0e1beef513080e72
evan <elingwange@gmail.com>
2019-04-28 21:14:06 +0800

    [LinkList]调试练习1

--- GrandKitTests/LinkListTests10.m
+++ GrandKitTests/LinkListTests10.m
@@ -24,38 +24,11 @@ typedef struct node {
  已知两单链表A和B分别表示两个集合，其元素值递增有序。
  试写一算法，求A和B的交集C，要求C同样以元素值递增的单链表形式存储。
  
- 用例1: A {1, 2, 3, 4}, B {2, 4, 6, 8} -> C {2, 4}
+ 用例1: A {1, 2, 3, 4}, B {2, 4, 6} -> C {2, 4}
  ...
  */
 
 @implementation LinkListTests10
-
-
-
-LinkList mixLinkList(LinkList A, LinkList B) {
-    // 创建链表C
-    LinkList C = (ListNode *)malloc(sizeof(ListNode));
-    ListNode *pc = C;
-    
-    for (ListNode *pa = A->next; pa->next != NULL; pa = pa->next) {
-        
-        for (ListNode *pb = B->next; pb->next != NULL; pb = pb->next) {
-            
-            DataType data = pa->data;
-            NSLog(@"Tag %d", data);
-            
-            if (pa->data == pb->data) {
-                ListNode *node = (ListNode *)malloc(sizeof(ListNode));
-                node->data = pb->data;
-                pc->next = node;
-                pc = pc->next;
-            }
-        }
-    }
-    return C;
-}
-
-
 
 
 LinkList creatLinkListA_10(int n) {
@@ -76,7 +49,7 @@ LinkList creatLinkListB_10(int n) {
     ListNode *head, *tail;
     head = (ListNode *)malloc(sizeof(ListNode));
     tail = head;
-    for (int i = 5; i < n; i += 2) {
+    for (int i = 2; i < n; i += 2) {
         ListNode *node = (ListNode *)malloc(sizeof(ListNode));
         node->data = i;
         tail->next = node;
@@ -97,16 +70,48 @@ void printLinkList_10(LinkList list) {
 }
 
 
+LinkList mixLinkList(LinkList A, LinkList B)
+{
+    ListNode *pa;
+    // 创建链表C
+    LinkList C = (ListNode *)malloc(sizeof(ListNode));
+    ListNode *pc = C;
+    
+    pa = A->next;
+    while (!(pa->data == 0 && pa->next == NULL))
+    {
+        ListNode *pb = B->next;
+        
+        while (pb->next != NULL)
+        {
+            if (pa->data == pb->data)
+            {
+                ListNode *node = (ListNode *)malloc(sizeof(ListNode));
+                node->data = pb->data;
+                pc->next = node;
+                pc = pc->next;
+            }
+            pb = pb->next;
+        }
+        pa = pa->next;
+    }
+    
+    
+    return C;
+}
+
+
+
 - (void)testLinkList_10 {
     
-    LinkList A = creatLinkListA_10(10);
+    LinkList A = creatLinkListA_10(5);
     printLinkList_10(A);
     
-    LinkList B = creatLinkListB_10(15);
+    LinkList B = creatLinkListB_10(8);
     printLinkList_10(B);
     
-    LinkList C = mixLinkList(A, B);
-    printLinkList_10(C);
+//    LinkList C = mixLinkList(A, B);
+//    printLinkList_10(C);
     
 }
 
