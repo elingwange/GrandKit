4dd65fc4c92cb8c979e236ade7523d2b0403ebbf
evan <elingwange@gmail.com>
2019-05-16 12:15:53 +0800

    [CircleSeqQueue]实现方法

--- GrandKitTests/DS/Queue/CircleSeqQueueTests.m
+++ GrandKitTests/DS/Queue/CircleSeqQueueTests.m
@@ -32,13 +32,50 @@ typedef char DataType;
 
 typedef struct queue {
     DataType data[MAX_SIZE];
-    
+    int top, rear;
 } CircleSeqQueue;
-
 
 /* ----- END ----- */
 
+CircleSeqQueue createCSQ() {
+    CircleSeqQueue queue;
+    queue.top = queue.rear = 0;
+    return queue;
+}
 
+/*
+ * 入队
+ */
+CircleSeqQueue * enqueueCSQ(CircleSeqQueue *queue, DataType x) {
+    // 判断CircleSeqQueue是否已满
+    if ((queue->rear + 1) % MAX_SIZE != queue->top) {
+        queue->data[queue->rear] = x;
+        queue->rear = (queue->rear + 1) % MAX_SIZE;
+    }
+    return queue;
+}
+
+/*
+ * 队列长度
+ */
+int lengthCSQ(CircleSeqQueue *queue) {
+    
+    // （rear - front + QueueSize）% QueueSize
+    
+    
+    
+    
+    return 0;
+}
+
+/*
+ * 出队
+ */
+CircleSeqQueue * dequeueCSQ(CircleSeqQueue *q, DataType *x) {
+    *x = q->data[q->top];
+    q->top = (q->top + 1) % MAX_SIZE;
+    return q;
+}
 
 
 @implementation CircleSeqQueueTests
@@ -46,6 +83,12 @@ typedef struct queue {
 
 - (void)testCircleSeqQueue {
     
+    CircleSeqQueue queue = createCSQ();
+    CircleSeqQueue *q = enqueueCSQ(&queue, 'A');
+    q = enqueueCSQ(&queue, 'A');
+    
+    DataType x;
+    q = dequeueCSQ(q, &x);
     
     
 }
